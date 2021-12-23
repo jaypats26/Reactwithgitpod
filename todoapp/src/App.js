4180b2f6ad5e6ed285f92f9914468e68d773e5c0
jaypats26 <jaypats26@gmail.com>
2021-12-23 12:14:34 +0000

    inialstudies

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,13 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>Hello!</div>
+  return ( <div>
+    <h1>My Todos</h1>
+    <Todo title="Learn React" />
+    <Todo title="Master React" />
+    <Todo title="Forget React" />
+  </div>
+  );
 }
 
 export default App;
