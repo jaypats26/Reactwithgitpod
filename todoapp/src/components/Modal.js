1498c901eb758237f5babc4175fe5db15b2e7083
jaypats26 <jaypats26@gmail.com>
2021-12-23 13:09:46 +0000

    Basicmodalstateandhandlerslearned

---
+++ todoapp/src/components/Modal.js
@@ -0,0 +1,14 @@
+function Modal(props){
+    function confirmHandler(){
+        props.onConfirm();
+    }
+    return (
+        <div className="modal">
+            <p>Are You Sure?</p>
+            <button className="btn btn--alt" onClick={props.onCancel}>Cancel</button>
+            <button className="btn" onClick={confirmHandler}>Confirm</button>
+        </div>
+    );
+}
+
+export default Modal;
--- todoapp/src/components/Todo.js
+++ todoapp/src/components/Todo.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props){
+    const [modelIsOpen, setModelIsOpen]=useState(false);
+
+    function deleteHandler(){
+        setModelIsOpen(true);
+    }
+
+    function closeModalHandler(){
+        setModelIsOpen(false);
+    }
+
     return (
         <div className='card'>
             <h2>{props.title}</h2>
             <div className='actions'>
-                <button className='btn'>Delete</button>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
             </div>
+            { modelIsOpen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> : null }
+            { modelIsOpen && <Backdrop onCancel={closeModalHandler}/> }
+            
         </div>
     );
 }
